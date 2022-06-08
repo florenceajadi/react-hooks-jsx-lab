66c28087de538fbdd4e4b3bc93253ce137134848
florence ajadi <florence.ajadi@yahoo.com>
2022-06-08 11:14:30 -0500

    commit tree f8991ed210e4952be63b0e9f35939ba28a968d75

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id = "home">
+      
+      <h1 style = {{color:'firebrick'}}>"{name} is a Web Developer from Your{city}"</h1>
+      
+    </div>
+  )
 }
 
 export default Home;
+ 
+
+// inside the div element, there should be one child element:
+// 1. an <h1> element, with the style attribute set to a color of "firebrick" and the content of "Your //Name is a Web Developer from Your City"
+// 2. use JSX expression syntax to use the imported name and city variables inside your JSX elements
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,20 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+ 
+  
+  )
 }
 
 export default NavBar;
+
+
+//1. a <nav> element
+// 2. inside the nav element, there should be two child elements:
+// 3. an <a> element, with the href attribute set to #home, and the content text set to "Home"
+// 4. an <a> element, with the href attribute set to #about, and the content text set to "About"
